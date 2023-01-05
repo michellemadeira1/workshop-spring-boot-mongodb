9d1d0fd1d197da1ab65bfe31ec05b913d11b5873
Michelle Madeira <michellemadeira1@gmail.com>
2023-01-05 14:45:57 -0300

    conectando ao MongoDB com repository e service

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+	
+	
+}
