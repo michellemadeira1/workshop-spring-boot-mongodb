b479f90487598e3199bacee02010d444d5b43053
Michelle Madeira <michellemadeira1@gmail.com>
2023-01-10 14:20:17 -0300

    Projescao dos dados do autor com DTO

--- src/main/java/config/Instantiation.java
+++ src/main/java/config/Instantiation.java
@@ -9,10 +9,10 @@ import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.repository.CrudRepository;
 
 import domain.Post;
 import domain.User;
+import dto.AuthorDTO;
 import repository.PostRepository;
 import repository.UserRepository;
 
@@ -38,8 +38,8 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2023"),"Partiu viagem","Vou viajar para São Paulo.Abraços!",maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2023"),"Bom dia!","Acordei feliz hoje!",maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2023"),"Partiu viagem","Vou viajar para São Paulo.Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2023"),"Bom dia!","Acordei feliz hoje!", new AuthorDTO( maria));
 		
 		userRepository.saveAll(Arrays.asList(maria, alex,bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
