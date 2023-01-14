ab91edd8150b812e8de3a7cfc9d0737fa9dce10f
Michelle Madeira <michellemadeira1@gmail.com>
2023-01-14 12:26:36 -0300

    Consulta com varios criterios

--- src/main/java/resources/PostResource.java
+++ src/main/java/resources/PostResource.java
@@ -1,5 +1,6 @@
 package resources;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,4 +39,18 @@ public class PostResource {
 		return ResponseEntity.ok().body(list); 
 	}
 
+
+	@GetMapping(value="fullsearch")
+	public ResponseEntity<List<Post>> fullsearch(
+			@RequestParam (value = "text", defaultValue="")String text,
+			@RequestParam (value = "minDate", defaultValue="")String minDate,
+			@RequestParam (value = "maxDate", defaultValue="")String maxDate){
+		text= URL.decodeParam(text);
+		Date min= URL.convertDate(minDate, new Date(0L));
+		Date max= URL.convertDate(maxDate, new Date());
+		List<Post>list = service.fullSearch(text,min,max);
+		return ResponseEntity.ok().body(list); 
+	}
+
+	
 }
--- src/main/java/services/PostService.java
+++ src/main/java/services/PostService.java
@@ -2,11 +2,13 @@ package services;
 
 
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import domain.Post;
 import repository.PostRepository;
 import services.exception.ObjectNotFoundException;
@@ -26,6 +28,11 @@ public class PostService {
 	}
 	public List <Post>findByTitle(String text){
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date (maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);		
 	}
 	
 }
